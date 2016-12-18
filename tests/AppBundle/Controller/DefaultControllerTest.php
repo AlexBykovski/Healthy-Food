f1c33b2906c31e09ff685ed5b7125371c36bed15
AlexBykovski <bykovski.work@gmail.com>
2016-12-19 00:48:39 +0300

    remove unused tests

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -7,20 +7,12 @@ use Symfony\Component\Yaml\Yaml;
 
 class DefaultControllerTest extends ControllerTestCase
 {
-    public function testIt()
-    {
-        $value = Yaml::parse("foo: bar");
-
-        $this->assertSame(['foo' => 'bar'], $value);
-        $this->assertSame(256, Yaml::PARSE_CONSTANT);
-    }
-    /*public function testIndex()
+    public function testIndex()
     {
         $client = $this->createNewClient();
 
         $crawler = $this->requestRoute("homepage");
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assert('HEALTHY FOOD', $crawler->filter('#container h1')->text());
-    }*/
+    }
 }
