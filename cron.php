a7d73db0fe5c262994ed21d7afb4242f6f2ae9a7
AlexBykovski <bykovski.work@gmail.com>
2017-04-21 00:20:59 +0300

    add cron file

---
+++ cron.php
@@ -0,0 +1,3 @@
+<?php
+//var_dump(file_get_contents("./app/Resources/views/email/remind-eating.html.twig", FILE_USE_INCLUDE_PATH));die;
+mail ("bykovski.work@gmail.com", "Healthy Food" , "message");
